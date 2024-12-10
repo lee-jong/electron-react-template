81714bec26cef9c3ad4ae9b0a99fae3fb32044b0
Bell <jay@clobot.co.kr>
2024-12-10 17:23:42 +0900

    [v0.0.1] optimizer.watchWindowShortcuts 인한 불필요 코드 제거

--- src/main/shortcut.ts
+++ src/main/shortcut.ts
@@ -1,12 +1,8 @@
 import { BrowserWindow, globalShortcut } from 'electron'
 
 const shortcutInit = (win: BrowserWindow) => {
-  globalShortcut.register('F12', () => {
-    console.log('prevent developer tools')
-  })
-  // 개발자 도구
-  globalShortcut.register('Alt+F12', () => {
-    win && win.webContents.openDevTools()
+  globalShortcut.register('F1', () => {
+    console.log('test', win)
   })
 }
 export default shortcutInit
